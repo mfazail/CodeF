53e58b7e941ac1a339cee6fd31fa61f29a5ce404
mfazail <80394548+mfazail@users.noreply.github.com>
2021-10-05 18:51:31 +0530

    Add files via upload

---
+++ NavBar/main.js
@@ -0,0 +1,12 @@
+function nav() {
+    return {
+        navOpen: false,
+        active: "home",
+        searchBar:false
+    }
+}
+function dropdown() {
+    return {
+        open:false
+    }
+}
